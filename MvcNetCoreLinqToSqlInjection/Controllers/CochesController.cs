60720181ccaf7c47c7d3808d90b3f445cad9da42
Jorgerdzz <asilgalan083@gmail.com>
2026-02-05 11:04:02 +0100

    Add project files.

---
+++ MvcNetCoreLinqToSqlInjection/Controllers/CochesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcNetCoreLinqToSqlInjection.Models;
+
+namespace MvcNetCoreLinqToSqlInjection.Controllers
+{
+    public class CochesController : Controller
+    {
+        private Coche coche;
+
+        public CochesController(Coche coche)
+        {
+            this.coche = coche;
+        }
+
+        public IActionResult Index()
+        {
+            return View(this.coche);
+        }
+
+        [HttpPost]
+        public IActionResult Index(string accion)
+        {
+            if(accion.ToLower() == "acelerar")
+            {
+                this.coche.Acelerar();
+            }else
+            {
+                this.coche.Frenar();
+            }
+            return View(this.coche);
+        }
+    }
+}
